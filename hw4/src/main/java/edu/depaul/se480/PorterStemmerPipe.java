433a4f1af953d4a672263f72b7b42fd2210ad0bf
rtalukder@forsythe.com <rtalukder@forsythe.com>
2019-11-07 21:37:17 -0600

    created data sink and porter stemmer classes

---
+++ hw4/src/main/java/edu/depaul/se480/PorterStemmerPipe.java
@@ -0,0 +1,10 @@
+package edu.depaul.se480;
+
+/**
+ * Class: SE480 - Pipe that utilizes the Porter Stemmer algorithm to
+ *                stem words to their morphological root
+ * Author: Raquib Talukder
+ **/
+
+public class PorterStemmerPipe {
+}
